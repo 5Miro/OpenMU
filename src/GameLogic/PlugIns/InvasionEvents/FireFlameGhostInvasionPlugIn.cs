0e4f856e2e5f191560298d84c7974e96ea7c34ae
Ramiro Vázquez <43798115+5Miro@users.noreply.github.com>
2025-11-26 23:12:13 -0300

    feat: Add Lunar Rabbit and Fire Flame Ghost invasion plugins
    Implement two new invasion event plugins:
    
    LunarRabbitInvasionPlugIn:
    - Spawns 10 Lunar Rabbits (ID: 413) on Devias and Elbeland maps
    - Runs every 4 hours starting from 02:30
    - Duration: 30 minutes
    
    FireFlameGhostInvasionPlugIn:
    - Spawns 10 Fire Flame Ghosts (ID: 463) on Devias and Elbeland maps
    - Runs every 4 hours starting from 03:00
    - Duration: 30 minutes
    
    Both plugins extend BaseInvasionPlugIn and include default configurations
    in PeriodicInvasionConfiguration with custom timetables and messages.

---
+++ src/GameLogic/PlugIns/InvasionEvents/FireFlameGhostInvasionPlugIn.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="FireFlameGhostInvasionPlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns.InvasionEvents;
+
+using System.Runtime.InteropServices;
+using MUnique.OpenMU.PlugIns;
+
+/// <summary>
+/// This plugin enables Fire Flame Ghost Invasion feature.
+/// </summary>
+[PlugIn(nameof(FireFlameGhostInvasionPlugIn), "Handle Fire Flame Ghost invasion event")]
+[Guid("24BB68B9-3014-4CC2-BDB5-53A9AE74D10A")]
+public class FireFlameGhostInvasionPlugIn : BaseInvasionPlugIn<PeriodicInvasionConfiguration>, ISupportDefaultCustomConfiguration
+{
+    private const ushort FireFlameGhostId = 463;
+
+    /// <summary>
+    /// Noria.
+    /// </summary>
+    protected const ushort ElbelandId = 51;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FireFlameGhostInvasionPlugIn"/> class.
+    /// </summary>
+    public FireFlameGhostInvasionPlugIn()
+        : base(null,
+            [
+                new(FireFlameGhostId, 10, MapId: DeviasId),
+                new(FireFlameGhostId, 10, MapId: ElbelandId),
+            ],
+            null)
+    {
+    }
+
+    /// <inheritdoc />
+    public object CreateDefaultConfig() => PeriodicInvasionConfiguration.DefaultFireFlameGhostInvasion;
+}
